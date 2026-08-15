d6aab8481972b871f5a48f93bea3dff2e4d2be9e
nichoth <nichoth@nichoth.com>
2026-08-15 11:01:26 -0700

    refactor qs and qsa

--- src/qs.ts
+++ src/qs.ts
@@ -1,1 +1,15 @@
-export const qs = document.querySelector.bind(document)
+export function qs<K extends keyof HTMLElementTagNameMap> (
+    selectors:K
+):HTMLElementTagNameMap[K]|null
+
+export function qs<K extends keyof SVGElementTagNameMap> (
+    selectors:K
+):SVGElementTagNameMap[K]|null
+
+export function qs<E extends Element = Element> (
+    selectors:string
+):E|null
+
+export function qs (selectors:string):Element|null {
+    return document.querySelector(selectors)
+}
--- src/qsa.ts
+++ src/qsa.ts
@@ -1,1 +1,15 @@
-export const qsa = document.querySelectorAll.bind(document)
+export function qsa<K extends keyof HTMLElementTagNameMap> (
+    selectors:K
+):NodeListOf<HTMLElementTagNameMap[K]>
+
+export function qsa<K extends keyof SVGElementTagNameMap> (
+    selectors:K
+):NodeListOf<SVGElementTagNameMap[K]>
+
+export function qsa<E extends Element = Element> (
+    selectors:string
+):NodeListOf<E>
+
+export function qsa (selectors:string):NodeListOf<Element> {
+    return document.querySelectorAll(selectors)
+}
